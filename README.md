1736a7b96719067c1c8c221fbd8a49e76c26a8bb
殷豪 <jjyyg1123@gmail.com>
2019-04-01 10:52:34 +0800

    fix: add default process.env

---
+++ README.md
@@ -0,0 +1,13 @@
+# Installation
+
+```
+npm install @jackyyin/redisasync
+```
+
+# process.env
+
+| Property | Default |
+|----------|---------|
+| REDIS_HOST | redis |
+| REDIS_PORT | 6379  |
+| REDIS_PASSWORD | null |
--- redis.js
+++ redis.js
@@ -4,8 +4,9 @@ const bluebird = require("bluebird");
 bluebird.promisifyAll(redis);
 
 const client = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
+  host: process.env.REDIS_HOST || 'redis',
+  port: process.env.REDIS_PORT || 6379,
+  password: process.env.REDIS_PASSWORD ? process.env.REDIS_PASSWORD : null
 });
 
 client.on('connect', () => {
